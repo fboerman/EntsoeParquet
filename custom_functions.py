4b3693f0cfa89d70d38e24d30b98471d262ac7d5
Frank Boerman <frank@fboerman.nl>
2024-10-28 23:11:44 +0100

    fix bug in custom fix netposition

--- custom_functions.py
+++ custom_functions.py
@@ -4,7 +4,8 @@ import pandas as pd
 def fix_netposition(df):
     df = pd.DataFrame(df[df['ContractType'] == 'Daily'])
     df['value'] = df.apply(lambda row: -1 * row['value'] if row['Direction'] == 'Import' else row['value'], axis=1)
-    df['mtu'] = df['mtu'].dt.floor('h', ambiguous='infer')
+    df['mtu'] = df['mtu'].dt.tz_convert('UTC')
+    df['mtu'] = df['mtu'].dt.floor('h')
     df = df.drop_duplicates(subset=['mtu', 'zone'], keep='first')
-
+    df['mtu'] = df['mtu'].dt.tz_convert('Europe/Amsterdam')
     return df.drop(columns=['ContractType', 'Direction'])
